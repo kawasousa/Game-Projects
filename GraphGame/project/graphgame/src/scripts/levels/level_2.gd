14458a51476d4b7ecb0e20f2cf3b615b329ef054
KaioGabriel-the <kkaiogabrielk@gmail.com>
2025-05-08 22:14:03 -0300

    ✨ feat/ level 2: construindo level 2.

---
+++ GraphGame/project/graphgame/src/scripts/levels/level_2.gd
@@ -0,0 +1,31 @@
+extends Node2D
+class_name Level_2
+
+signal newError;
+signal graphHovered(minimumObstacles: int);
+
+var time: int = 1;
+var errors: int = 0;
+@onready var timer: Timer = $Timer
+@onready var board: Node2D = $Board
+@export var graphs: Array[Graph] = [];
+@export var title: String = "Nível Normal";
+
+
+func _ready() -> void:
+	Game.emitLevelStarted(self);
+	Sound.playMusic(MusicDB.SEA_THEME_1, true);
+	
+	for graph in graphs:
+		graph.newError.connect(emitNewError);
+		graph.hovered.connect(func(obstacles: int): graphHovered.emit(obstacles));
+	
+	timer.start();
+	timer.timeout.connect(onTimerTimeout);
+
+func emitNewError():
+	errors += 1;
+	newError.emit();
+
+func onTimerTimeout():
+	time += 1;
